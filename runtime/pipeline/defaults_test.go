49599a166f8ba1273c522619acccf36e230f5773
soonkuk <soonkuk@gmail.com>
2026-09-01 11:37:55 +0900

    refactor: align node and API assembly with nft-model

--- runtime/pipeline/defaults_test.go
+++ runtime/pipeline/defaults_test.go
@@ -1,38 +1,16 @@
 package pipeline
 
 import (
-	"context"
 	"strings"
 	"testing"
 
 	"github.com/imfact-labs/mitum2/util/ps"
-	"github.com/imfact-labs/smart-contract-model/runtime/steps"
 )
 
 func TestDefaultImportPipelineUsesSmartContractHinterStep(t *testing.T) {
 	pps := DefaultImportPS()
 	if !containsProcessName(pps.Verbose(), "add-hinters") {
 		t.Fatalf("import pipeline does not contain hinter hook: %v", pps.Verbose())
-	}
-}
-
-func TestDefaultRunPipelineIncludesSmartContractProcessorStep(t *testing.T) {
-	noop := func(ctx context.Context) (context.Context, error) { return ctx, nil }
-	pps := DefaultRunPS(RunHooks{
-		DigesterName:             ps.Name("test-smart-digester"),
-		Digester:                 noop,
-		StartDigesterName:        ps.Name("test-smart-start-digester"),
-		StartDigester:            noop,
-		CheckHold:                noop,
-		ProposalProcessors:       noop,
-		WhenNewBlockSaved:        noop,
-		WhenNewBlockConfirmed:    noop,
-		WhenNewBlockSavedSyncing: noop,
-		DigestAPIHandlers:        noop,
-		DigesterFollowUp:         noop,
-	})
-	if !containsProcessName(pps.Verbose(), steps.PNameOperationProcessorsMap.String()) {
-		t.Fatalf("run pipeline does not contain processor step: %v", pps.Verbose())
 	}
 }
 
